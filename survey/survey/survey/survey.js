3ffef5ea41c7041b80353966ffeef3cb39a81eec
varun-101 <122275541+varun-101@users.noreply.github.com>
2023-10-05 10:34:19 +0530

    Add files via upload

---
+++ survey/survey/survey/survey.js
@@ -0,0 +1,164 @@
+(function() {
+    var questions = [{
+      question: "What do you feel about your Sales Target?",
+      choices: ['I consistently meet or exceed my monthly sales targets','I am confident in my ability to achieve my sales goals','I believe my performance positively contributes to our sales growth'],
+    }, {
+      question: "What do you feel about your Productivity?",
+      choices: ['I am able to complete my tasks efficiently', 'I consistently meet project deadlines', 'I feel productive in my role'],
+    }, {
+      question: "What are your views on customer satisfaction?",
+      choices: ['I prioritize providing excellent service to our customer', 'I believe our customers are generally satisfied with my interaction', 'I believe our customers are generally satisfied with my interaction'],
+     
+    }, {
+      question: "How is your quality of work?",
+      choices: ['I take pride in the quality of my work','My work is consistently accurate and error-free', 'I maintain high standards for the quality of my output'],
+    
+    }, {
+      question: "How do you feel about your work engagement?",
+      choices: ['I have a sense of purpose and belonging within the organization', 'I feel engaged and motivated in my role', 'I am enthusiastic about contributing to the companys success'],
+      correctAnswer: 4
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('Thanks for submitting your response');
+      return score;
+    }
+  })();
